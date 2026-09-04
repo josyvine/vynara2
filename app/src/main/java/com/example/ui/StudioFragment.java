83ac2cc97548365223a3276c07d6a296094d6975
josyvine <64621103+josyvine@users.noreply.github.com>
2026-09-04 12:34:56 +0530

    Update StudioFragment.java

--- app/src/main/java/com/example/ui/StudioFragment.java
+++ app/src/main/java/com/example/ui/StudioFragment.java
@@ -279,7 +279,9 @@ public class StudioFragment extends Fragment {
 
             for (Character ch : result.getCharacters()) {
                 runtime.getCharacterManager().registerCharacter(ch);
-                engine.getSceneManager().getActiveScene().addObject(new SceneObject(ch.getName(), ch.getMesh()));
+                if (ch.getRootObject() != null) {
+                    engine.getSceneManager().getActiveScene().addObject(ch.getRootObject());
+                }
             }
 
             engine.getSceneManager().updateWorldTransforms();
